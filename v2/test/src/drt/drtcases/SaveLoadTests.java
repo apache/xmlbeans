2ac02a54628eb325653500cb583ec87899febe60
Cezar Cristian Andrei <cezar@apache.org>
2003-11-14 20:28:36 +0000

    Two new utilities: a loader to a generic DOM and a saver from an XmlStreamReader. These will be used  in the first phase on the new implementation of the xml store.
    CR: Eric
    DRT passing.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/xmlbeans/trunk@110245 13f79535-47bb-0310-9956-ffa450edef68

--- v2/test/src/drt/drtcases/SaveLoadTests.java
+++ v2/test/src/drt/drtcases/SaveLoadTests.java
@@ -54,41 +54,60 @@
 * Foundation, please see <http://www.apache.org/>.
 */
 
+/**
+ * Author: Cezar Andrei ( cezar.andrei at bea.com )
+ * Date: Nov 13, 2003
+ */
 package drtcases;
+
+import org.w3c.dom.Document;
+import org.apache.xmlbeans.impl.common.LoadSaveUtils;
+import org.apache.xmlbeans.XmlObject;
+import org.apache.xmlbeans.XmlException;
+import org.xml.sax.SAXException;
+
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.io.IOException;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
 
 import junit.framework.Test;
 import junit.framework.TestSuite;
 import junit.framework.TestCase;
+import junit.framework.Assert;
 
-public class SmokeTests extends TestCase
+public class SaveLoadTests extends TestCase
 {
-    SmokeTests(String name) { super(name); }
+    public SaveLoadTests(String name) { super(name); }
+    public static Test suite() { return new TestSuite(SaveLoadTests.class); }
 
-    public static Test suite()
+    public void testLoadSave()
+            throws IOException, SAXException, ParserConfigurationException, XmlException, XMLStreamException
     {
-        TestSuite suite = new TestSuite(SmokeTests.class.getName());
-        suite.addTest(AssortedTests.suite());
-        suite.addTest(IntTests.suite());
-        suite.addTest(RuntimeSchemaLoaderTest.suite());
-        suite.addTest(StoreTests.suite());
-        suite.addTest(QNameTests.suite());
-        suite.addTest(ValidationTests.suite());
-        suite.addTest(CompilationTests.suite());
-        suite.addTest(AnnotationsTests.suite());
-        suite.addTest(EasyPoTests.suite());
-        suite.addTest(NameworldTest.suite());
-        suite.addTest(SchemaTypesTests.suite());
-        suite.addTest(EnumTests.suite());
-        suite.addTest(CreationTests.suite());
-        suite.addTest(ThreadingTest.suite());
-        suite.addTest(SerializationTests.suite());
-        suite.addTest(DomTests.suite());
-        suite.addTest(GDateTests.suite());
-        suite.addTest(SubstGroupTests.suite());
-        suite.addTest(BindingTests.suite());
-        suite.addTest(JaxenXPathTests.suite());
-        suite.addTest(MarshalTests.suite());
-        suite.addTest(SaveLoadTests.suite());
-        return suite;
+        File file = TestEnv.xbeanCase("xpath/testXPath.xml");
+
+        Document doc = LoadSaveUtils.xmlText2GenericDom(new FileInputStream(file),
+                DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument());
+
+        XmlObject xo = XmlObject.Factory.parse(doc);
+
+        XMLStreamReader xsr = xo.newXMLStreamReader();
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        LoadSaveUtils.xmlStreamReader2XmlText(xsr, bos);
+
+        check( XmlObject.Factory.parse(TestEnv.xbeanCase("xpath/testXPath.xml")).toString(), bos.toString() );
+    }
+
+    private static void check(String expected, String actual) throws XmlException
+    {
+        XmlComparator.Diagnostic diag = new XmlComparator.Diagnostic();
+        boolean match = XmlComparator.lenientlyCompareTwoXmlStrings(actual, expected, diag);
+        Assert.assertTrue("------------  Found difference:" +
+                " actual=\n'" + actual + "'\nexpected=\n'" + expected + "'\ndiagnostic=" + diag , match);
     }
 }
