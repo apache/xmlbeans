2ffbd18e7d99660c04b5d8325e41f26864948036
Kenneth Eric Vasilik <ericvas@apache.org>
2004-01-08 19:49:40 +0000

    Misc namesapce changes
    git-svn-id: https://svn.apache.org/repos/asf/xmlbeans/trunk@110318 13f79535-47bb-0310-9956-ffa450edef68

--- v2/src/newstore/org/apache/xmlbeans/impl/newstore/CharUtil.java
+++ v2/src/newstore/org/apache/xmlbeans/impl/newstore/CharUtil.java
@@ -1,3 +1,59 @@
+/*
+* The Apache Software License, Version 1.1
+*
+*
+* Copyright (c) 2003 The Apache Software Foundation.  All rights 
+* reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met:
+*
+* 1. Redistributions of source code must retain the above copyright
+*    notice, this list of conditions and the following disclaimer. 
+*
+* 2. Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in
+*    the documentation and/or other materials provided with the
+*    distribution.
+*
+* 3. The end-user documentation included with the redistribution,
+*    if any, must include the following acknowledgment:  
+*       "This product includes software developed by the
+*        Apache Software Foundation (http://www.apache.org/)."
+*    Alternately, this acknowledgment may appear in the software itself,
+*    if and wherever such third-party acknowledgments normally appear.
+*
+* 4. The names "Apache" and "Apache Software Foundation" must 
+*    not be used to endorse or promote products derived from this
+*    software without prior written permission. For written 
+*    permission, please contact apache@apache.org.
+*
+* 5. Products derived from this software may not be called "Apache 
+*    XMLBeans", nor may "Apache" appear in their name, without prior 
+*    written permission of the Apache Software Foundation.
+*
+* THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+* WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+* OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
+* ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+* SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+* LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+* USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+* OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+* SUCH DAMAGE.
+* ====================================================================
+*
+* This software consists of voluntary contributions made by many
+* individuals on behalf of the Apache Software Foundation and was
+* originally based on software copyright (c) 2000-2003 BEA Systems 
+* Inc., <http://www.bea.com/>. For more information on the Apache Software
+* Foundation, please see <http://www.apache.org/>.
+*/
+
 package org.apache.xmlbeans.impl.newstore;
 
 import java.util.ArrayList;
@@ -11,12 +67,7 @@ public final class CharUtil
         return (CharUtil) tl_charUtil.get();
     }
     
-    private CharUtil ( )
-    {
-        this( 1024 * 32 );
-    }
-    
-    private CharUtil ( int charBufSize )
+    public CharUtil ( int charBufSize )
     {
         _charBufSize = charBufSize;
     }
@@ -314,33 +365,47 @@ public final class CharUtil
         // or just create a new string ... this goes for remove chars
         // as well.
 
-        _cchSrc = cch + cchInsert;
-
         if (cchInsert == 0)
         {
             _cchSrc = cch;
             _offSrc = off;
+            
             return src;
         }
 
         _cchSrc = cch + cchInsert;
-        _offSrc = 0;
 
         Object newSrc;
-        
-        if (posInsert == 0)
-            newSrc = new CharJoin( src, off, cch, srcInsert, offInsert, cchInsert );
 
-        if (posInsert == cch)
-            newSrc = new CharJoin( srcInsert, offInsert, cchInsert, src, off, cch );
+        if (_cchSrc <= 32 && canAllocate( _cchSrc ))
+        {
+            char[] chars = allocate( _cchSrc );
+
+            getChars( chars, _offSrc, src, off, posInsert );
+            
+            getChars( chars, _offSrc + posInsert, srcInsert, offInsert, cchInsert );
+            
+            getChars(
+                chars, _offSrc + posInsert + cchInsert, src, off + posInsert, cch - posInsert );
+
+            newSrc = chars;
+        }
         else
         {
-
-            newSrc =
-                new CharJoin( 
-                    new CharJoin( src, off, posInsert, srcInsert, offInsert, cchInsert ),
-                    0, posInsert + cchInsert,
-                    src, off + posInsert, cch - posInsert );
+            _offSrc = 0;
+            
+            if (posInsert == 0)
+                newSrc = new CharJoin( src, off, cch, srcInsert, offInsert, cchInsert );
+            else if (posInsert == cch)
+                newSrc = new CharJoin( srcInsert, offInsert, cchInsert, src, off, cch );
+            else
+            {
+                newSrc =
+                    new CharJoin( 
+                        new CharJoin( src, off, posInsert, srcInsert, offInsert, cchInsert ),
+                        0, posInsert + cchInsert,
+                        src, off + posInsert, cch - posInsert );
+            }
         }
 
         assert isValid( newSrc, _offSrc, _cchSrc );
@@ -369,10 +434,28 @@ public final class CharUtil
         }
         else if (posRemove + cchRemove != cch)
         {
-            newSrc = 
-                new CharJoin(
-                    src, off, posRemove,
+            int cchAfter = cch - cchRemove;
+            
+            if (cchAfter <= 32 && canAllocate( cchAfter ))
+            {
+                char[] chars = allocate( cchAfter );
+
+                getChars( chars, _offSrc, src, off, posRemove );
+
+                getChars(
+                    chars, _offSrc + posRemove,
                     src, off + posRemove + cchRemove, cch - posRemove - cchRemove );
+
+                newSrc = chars;
+                newOff = 0;
+            }
+            else
+            {
+                newSrc = 
+                    new CharJoin(
+                        src, off, posRemove,
+                        src, off + posRemove + cchRemove, cch - posRemove - cchRemove );
+            }
             
             newOff = 0;
         }
@@ -390,6 +473,11 @@ public final class CharUtil
         return newSrc;
     }
 
+    private boolean canAllocate ( int cch )
+    {
+        return _currentBuffer == null || _currentBuffer.length - _currentOffset >= cch;
+    }
+    
     private char[] allocate ( int cch )
     {
         assert _currentBuffer == null || _currentBuffer.length - _currentOffset > 0;
@@ -584,7 +672,7 @@ public final class CharUtil
     }
     
     private static ThreadLocal tl_charUtil =
-        new ThreadLocal() { protected Object initialValue() { return new CharUtil(); } };
+        new ThreadLocal() { protected Object initialValue() { return new CharUtil( 1024 * 32 ); } };
 
     public int _offSrc;
     public int _cchSrc;
