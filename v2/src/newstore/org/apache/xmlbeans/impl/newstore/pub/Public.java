5ed2bba4775c7fbe69002d9a7673a81176b55f80
Kenneth Eric Vasilik <ericvas@apache.org>
2004-02-22 03:36:44 +0000

    removed newstore in favor of newstore2
    git-svn-id: https://svn.apache.org/repos/asf/xmlbeans/trunk@110448 13f79535-47bb-0310-9956-ffa450edef68

--- v2/src/newstore/org/apache/xmlbeans/impl/newstore/pub/Public.java
+++ v2/src/newstore/org/apache/xmlbeans/impl/newstore/pub/Public.java
@@ -37,111 +37,111 @@ import java.io.InputStream;
 
 public class Public
 {
-    public static class MemoryBackend implements Backend
-    {
-        public Locale newLocale ( )
-        {
-            return new Mcur.Mlocale();
-        }
-    }
-    
-    public static Backend memoryBackend ( )
-    {
-        return new MemoryBackend();
-    }
-    
-    public static Locale getLocale ( Backend be )
-    {
-        return be.newLocale();
-    }
-    
-    public static DOMImplementation getImplementation ( Saaj saaj, Backend be )
-    {
-        Locale l = getLocale( be );
-
-        if (saaj != null)
-        {
-            l._saaj = saaj;
-            saaj.setCallback( l );
-        }
-
-        return (DOMImplementation) l;
-    }
-    
-    public static DOMImplementation getImplementation ( Backend be )
-    {
-        return (DOMImplementation) getLocale( be );
-    }
-
-    public static Document parse ( String s, Backend be )
-    {
-        Locale l = getLocale( be );
-
-        Dom d;
-
-        if (l.noSync())         { l.enter(); try { d = l.load( s ); } finally { l.exit(); } }
-        else synchronized ( l ) { l.enter(); try { d = l.load( s ); } finally { l.exit(); } }
-
-        return (Document) d;
-    }
-
-    public static Document parse ( String s, Backend be, Saaj saaj )
-    {
-        Locale l = getLocale( be );
-
-        if (saaj != null)
-        {
-            l._saaj = saaj;
-            saaj.setCallback( l );
-        }
-
-        Dom d;
-
-        if (l.noSync())         { l.enter(); try { d = l.load( s ); } finally { l.exit(); } }
-        else synchronized ( l ) { l.enter(); try { d = l.load( s ); } finally { l.exit(); } }
-
-        return (Document) d;
-    }
-
-    public static Document parse ( InputStream is, Backend be, Saaj saaj )
-    {
-        Locale l = getLocale( be );
-
-        if (saaj != null)
-        {
-            l._saaj = saaj;
-            saaj.setCallback( l );
-        }
-
-        Dom d;
-
-        if (l.noSync())         { l.enter(); try { d = l.load( is ); } finally { l.exit(); } }
-        else synchronized ( l ) { l.enter(); try { d = l.load( is ); } finally { l.exit(); } }
-
-        return (Document) d;
-    }
-
-    public static XMLStreamReader getStream ( Node n )
-    {
-        assert n instanceof Dom;
-
-        Dom d = (Dom) n;
-        
-        Locale l = d.locale();
-
-        if (l.noSync())         { l.enter(); try { return DomImpl.getXmlStreamReader( d ); } finally { l.exit(); } }
-        else synchronized ( l ) { l.enter(); try { return DomImpl.getXmlStreamReader( d ); } finally { l.exit(); } }
-    }
-
-    public static XmlCursor getCursor ( Node n )
-    {
-        assert n instanceof Dom;
-
-        Dom d = (Dom) n;
-
-        Locale l = d.locale();
-
-        if (l.noSync())         { l.enter(); try { return DomImpl.getXmlCursor( d ); } finally { l.exit(); } }
-        else synchronized ( l ) { l.enter(); try { return DomImpl.getXmlCursor( d ); } finally { l.exit(); } }
-    }
+//    public static class MemoryBackend implements Backend
+//    {
+//        public Locale newLocale ( )
+//        {
+//            return new Mcur.Mlocale();
+//        }
+//    }
+//    
+//    public static Backend memoryBackend ( )
+//    {
+//        return new MemoryBackend();
+//    }
+//    
+//    public static Locale getLocale ( Backend be )
+//    {
+//        return be.newLocale();
+//    }
+//    
+//    public static DOMImplementation getImplementation ( Saaj saaj, Backend be )
+//    {
+//        Locale l = getLocale( be );
+//
+//        if (saaj != null)
+//        {
+//            l._saaj = saaj;
+//            saaj.setCallback( l );
+//        }
+//
+//        return (DOMImplementation) l;
+//    }
+//    
+//    public static DOMImplementation getImplementation ( Backend be )
+//    {
+//        return (DOMImplementation) getLocale( be );
+//    }
+//
+//    public static Document parse ( String s, Backend be )
+//    {
+//        Locale l = getLocale( be );
+//
+//        Dom d;
+//
+//        if (l.noSync())         { l.enter(); try { d = l.load( s ); } finally { l.exit(); } }
+//        else synchronized ( l ) { l.enter(); try { d = l.load( s ); } finally { l.exit(); } }
+//
+//        return (Document) d;
+//    }
+//
+//    public static Document parse ( String s, Backend be, Saaj saaj )
+//    {
+//        Locale l = getLocale( be );
+//
+//        if (saaj != null)
+//        {
+//            l._saaj = saaj;
+//            saaj.setCallback( l );
+//        }
+//
+//        Dom d;
+//
+//        if (l.noSync())         { l.enter(); try { d = l.load( s ); } finally { l.exit(); } }
+//        else synchronized ( l ) { l.enter(); try { d = l.load( s ); } finally { l.exit(); } }
+//
+//        return (Document) d;
+//    }
+//
+//    public static Document parse ( InputStream is, Backend be, Saaj saaj )
+//    {
+//        Locale l = getLocale( be );
+//
+//        if (saaj != null)
+//        {
+//            l._saaj = saaj;
+//            saaj.setCallback( l );
+//        }
+//
+//        Dom d;
+//
+//        if (l.noSync())         { l.enter(); try { d = l.load( is ); } finally { l.exit(); } }
+//        else synchronized ( l ) { l.enter(); try { d = l.load( is ); } finally { l.exit(); } }
+//
+//        return (Document) d;
+//    }
+//
+//    public static XMLStreamReader getStream ( Node n )
+//    {
+//        assert n instanceof Dom;
+//
+//        Dom d = (Dom) n;
+//        
+//        Locale l = d.locale();
+//
+//        if (l.noSync())         { l.enter(); try { return DomImpl.getXmlStreamReader( d ); } finally { l.exit(); } }
+//        else synchronized ( l ) { l.enter(); try { return DomImpl.getXmlStreamReader( d ); } finally { l.exit(); } }
+//    }
+//
+//    public static XmlCursor getCursor ( Node n )
+//    {
+//        assert n instanceof Dom;
+//
+//        Dom d = (Dom) n;
+//
+//        Locale l = d.locale();
+//
+//        if (l.noSync())         { l.enter(); try { return DomImpl.getXmlCursor( d ); } finally { l.exit(); } }
+//        else synchronized ( l ) { l.enter(); try { return DomImpl.getXmlCursor( d ); } finally { l.exit(); } }
+//    }
 }
