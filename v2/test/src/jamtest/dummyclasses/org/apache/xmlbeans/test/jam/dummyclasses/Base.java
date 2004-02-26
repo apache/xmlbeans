985619d2e3515a26195a51ad81f395735fae8f35
Patrick Calahan <pcal@apache.org>
2004-02-26 23:22:50 +0000

    jam: fix test class
    git-svn-id: https://svn.apache.org/repos/asf/xmlbeans/trunk@110465 13f79535-47bb-0310-9956-ffa450edef68

--- v2/test/src/jamtest/dummyclasses/org/apache/xmlbeans/test/jam/dummyclasses/Base.java
+++ v2/test/src/jamtest/dummyclasses/org/apache/xmlbeans/test/jam/dummyclasses/Base.java
@@ -9,8 +9,7 @@ package org.apache.xmlbeans.test.jam.dummyclasses;
  *
  *  @author pcal Nov 25, 2003
  */
-public abstract class Base extends Foo,
- {
+public abstract class Base implements Foo  {
 
   /**
    * NOT doonkie javadoc! 
@@ -23,35 +22,10 @@ public abstract class Base extends Foo,
 
   public int getId() { return -1; }
 
-  public double getId2();
 
   public String getFoo() { return "foo"; } // {{}}{{
 
   public void setId(int id) {}
 
-  public void setSomething(int x, 
-foo bii,
-Object dss);
-
-  /**
-   * Creates a new member in this Annotation definition and returns the
-   * result.
-   *
-   * @param type JClass representing the type of the new member.
-   * @param name A name for the new member.
-   * @param dflt A default value for the new member.  Primitives should be
-   * wrapped in java.lang wrappers, e.g. java.lang.Integer.
-   *
-   * @return The newly-added EAnnotation.
-   */
-  public EAnnotationMemberDefinition addNewMemberDefinition(JClass type,
-                                                            String name,
-                                                            Object dflt);
-
-  /**
-   *
-   * @param memberDef
-   */
-  public void removeMemberDefinition(EAnnotationMemberDefinition memberDef);
 
 }
