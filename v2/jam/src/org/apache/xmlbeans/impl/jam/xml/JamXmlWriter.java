ff64206116f03b6f770b72b05a511ed2517fbdf0
Patrick Calahan <pcal@apache.org>
2004-03-15 22:30:34 +0000

    stage jam and jam_old in xbean.jar
    git-svn-id: https://svn.apache.org/repos/asf/xmlbeans/trunk@110570 13f79535-47bb-0310-9956-ffa450edef68

---
+++ v2/jam/src/org/apache/xmlbeans/impl/jam/xml/JamXmlWriter.java
@@ -0,0 +1,62 @@
+/*   Copyright 2004 The Apache Software Foundation
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.xmlbeans.impl.jam.visitor;
+
+import org.apache.xmlbeans.impl.jam.JPackage;
+import org.apache.xmlbeans.impl.jam.JClass;
+import org.apache.xmlbeans.impl.jam.JConstructor;
+import org.apache.xmlbeans.impl.jam.JField;
+import org.apache.xmlbeans.impl.jam.JMethod;
+import org.apache.xmlbeans.impl.jam.JParameter;
+import org.apache.xmlbeans.impl.jam.JAnnotation;
+import org.apache.xmlbeans.impl.jam.JComment;
+import org.apache.xmlbeans.impl.jam.JAnnotatedElement;
+import org.apache.xmlbeans.impl.jam.JInvokable;
+
+import java.io.Writer;
+
+/**
+ * @author Patrick Calahan &lt;email: pcal-at-bea-dot-com&gt;
+ */
+public class JamXmlWriter extends JVisitor {
+  /*
+  // ========================================================================
+  // Constants
+
+  private static final String PACKAGE_ELEMENT = "package";
+  private static final String CLASS_ELEMENT = "class";  
+
+  // ========================================================================
+  // Variables
+
+  private XMLStreamWriter mOut;
+
+  // ========================================================================
+  // Constructors
+
+  public JamXmlWriter(Writer out) throws XMLStreamException  {
+    if (out == null) throw new IllegalArgumentException("null out");
+    mOut = XMLOutputFactory.newInstance().createXMLStreamWriter(out);
+  }
+
+  public JamXmlWriter(XMLStreamWriter out) {
+    if (out == null) throw new IllegalArgumentException("null out");
+    mOut = out;
+  }
+  */
+}
+
+
+
