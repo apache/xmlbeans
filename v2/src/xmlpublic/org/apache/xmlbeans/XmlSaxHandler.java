b518afb1a0e74504ce1eb2d5662827ede44eb778
Kenneth Eric Vasilik <ericvas@apache.org>
2004-06-29 19:23:16 +0000

    Add ability for XmlSaxHandler to insert bookmarks while loading
    git-svn-id: https://svn.apache.org/repos/asf/xmlbeans/trunk@110855 13f79535-47bb-0310-9956-ffa450edef68

--- v2/src/xmlpublic/org/apache/xmlbeans/XmlSaxHandler.java
+++ v2/src/xmlpublic/org/apache/xmlbeans/XmlSaxHandler.java
@@ -15,6 +15,8 @@
 
 package org.apache.xmlbeans;
 
+import javax.xml.namespace.QName;
+
 import org.xml.sax.ContentHandler;
 import org.xml.sax.ext.LexicalHandler;
 
@@ -33,11 +35,22 @@ public interface XmlSaxHandler
      * @see ContentHandler
      */ 
     ContentHandler getContentHandler ( );
+    
     /**
      * The LexicalHandler expecting SAX lexical events.
      * @see LexicalHandler
      */ 
     LexicalHandler getLexicalHandler ( );
+    
+    /**
+     * Insert a bookmark before the token associated with the last SAX event.
+     */ 
+    void bookmarkLastEvent ( XmlCursor.XmlBookmark mark );
+    
+    /**
+     * Insert a bookmark before the attr token associated with the last SAX element event.
+     */ 
+    void bookmarkLastAttr ( QName attrName, XmlCursor.XmlBookmark mark );
     
     /** Returns the loaded XmlObject after all the SAX events have been finished */
     XmlObject getObject ( ) throws XmlException;
