647b53369d610f3ba7ce275596b91c2aab5471f8
Cezar Cristian Andrei <cezar@apache.org>
2004-11-09 20:42:24 +0000

    Fix for sDownload tool. Added correct v2 classpath entries. Submitted by: Jacob Danner
    git-svn-id: https://svn.apache.org/repos/asf/xmlbeans/trunk@111095 13f79535-47bb-0310-9956-ffa450edef68

--- v2/bin/sdownload.cmd
+++ v2/bin/sdownload.cmd
@@ -9,6 +9,9 @@ if "%XMLBEANS_HOME%" EQU "" (set XMLBEANS_HOME=%~dp0..)
 
 set cp=
 set cp=%cp%;%XMLBEANS_HOME%\build\ar\xbean.jar
+set cp=%cp%;%XMLBEANS_HOME%\build\lib\jsr173_api.jar
+set cp=%cp%;%XMLBEANS_HOME%\build\lib\resolver.jar
+
 
 java -classpath %cp% org.apache.xmlbeans.impl.tool.SchemaResourceManager %*
 
