9725a5af18f4e8f2c98deb2e1621bfe8098c463f
Kevin Krouse <kkrouse@apache.org>
2005-01-18 21:50:50 +0000

    mem perf tests Contributed by Jamie Zyskowski
    git-svn-id: https://svn.apache.org/repos/asf/xmlbeans/trunk@125551 13f79535-47bb-0310-9956-ffa450edef68

--- test/perf/bin/run_v2.bat
+++ test/perf/bin/run_v2.bat
@@ -1,90 +1,65 @@
-echo off
-call v2.bat POReadAllV2 1 
-echo
-call v2.bat POReadAllV2 2 
-echo
-call v2.bat POReadAllV2 3 
-echo
-call v2.bat POReadAllV2 4 
-echo
-call v2.bat POReadAllV2 5 
-echo
-call v2.bat POReadAllV2 6 
-echo
-call v2.bat POReadAllV2 7 
-echo
-call v2.bat POReadOneV2 1
-echo
-call v2.bat POReadOneV2 2
-echo
-call v2.bat POReadOneV2 3
-echo
-call v2.bat POReadOneV2 4
-echo
-call v2.bat POReadOneV2 5
-echo
-call v2.bat POReadOneV2 6
-echo
-call v2.bat POReadOneV2 7
-echo
+call v2.bat POReadAllV2 filename 1 
+call v2.bat POReadAllV2 filename 2 
+call v2.bat POReadAllV2 filename 3 
+call v2.bat POReadAllV2 filename 4 
+call v2.bat POReadAllV2 filename 5 
+call v2.bat POReadAllV2 filename 6 
+call v2.bat POReadAllV2 filename 7 
+call v2.bat POReadOneV2 filename 1
+call v2.bat POReadOneV2 filename 2
+call v2.bat POReadOneV2 filename 3
+call v2.bat POReadOneV2 filename 4
+call v2.bat POReadOneV2 filename 5
+call v2.bat POReadOneV2 filename 6
+call v2.bat POReadOneV2 filename 7
 call v2.bat POTopDownV2
-echo
 call v2.bat POTopDownSaveV2
-echo
 call v2.bat POValidateXmlObjV2
-echo
 call v2.bat PrimTopDownV2
-echo 
 call v2.bat NPrimTopDownV2
-echo 
-call v2.bat CursorWalkV2 deep-attributes
-echo
-call v2.bat CursorWalkV2 deep-elements
-echo 
-call v2.bat CursorWalkV2 wide-attributes
-echo
-call v2.bat CursorWalkV2 wide-elements
-echo
-call v2.bat CursorWalkV2 wide-text
-echo
-call v2.bat CursorGetAttributeV2 deep-attributes
-echo
-call v2.bat CursorGetAttributeV2 wide-attributes
-echo
-call v2.bat CursorGetSetGetAttributeV2 deep-attributes
-echo
-call v2.bat CursorGetSetGetAttributeV2 wide-attributes
-echo
+call v2.bat CursorWalkV2 flavor deep-attributes
+call v2.bat CursorWalkV2 flavor deep-elements
+call v2.bat CursorWalkV2 flavor wide-attributes
+call v2.bat CursorWalkV2 flavor wide-elements
+call v2.bat CursorWalkV2 flavor wide-text
+call v2.bat CursorGetAttributeV2 flavor deep-attributes
+call v2.bat CursorGetAttributeV2 flavor wide-attributes
+call v2.bat CursorGetSetGetAttributeV2 flavor deep-attributes
+call v2.bat CursorGetSetGetAttributeV2 flavor wide-attributes
 call v2.bat CursorGetElementV2
-echo
 call v2.bat CursorGetSetGetElementV2
-echo
-call v2.bat BisLoadV2 deep-attributes
-echo 
-call v2.bat BisLoadV2 deep-elements
-echo 
-call v2.bat BisLoadV2 wide-attributes
-echo 
-call v2.bat BisLoadV2 wide-elements
-echo 
-call v2.bat BisLoadV2 wide-text
-echo 
-call v2.bat DomWalkV2 deep-attributes
-echo
-call v2.bat DomWalkV2 deep-elements
-echo
-call v2.bat DomWalkV2 wide-attributes
-echo
-call v2.bat DomWalkV2 wide-elements
-echo
-call v2.bat DomWalkV2 wide-text
-echo
-call v2.bat DomWalkNLV2 deep-attributes
-echo
-call v2.bat DomWalkNLV2 deep-elements
-echo
-call v2.bat DomWalkNLV2 wide-attributes
-echo
-call v2.bat DomWalkNLV2 wide-elements
-echo
-call v2.bat DomWalkNLV2 wide-text
+call v2.bat BisLoadV2 flavor deep-attributes
+call v2.bat BisLoadV2 flavor deep-elements
+call v2.bat BisLoadV2 flavor wide-attributes
+call v2.bat BisLoadV2 flavor wide-elements
+call v2.bat BisLoadV2 flavor wide-text
+call v2.bat DomWalkV2 flavor deep-attributes
+call v2.bat DomWalkV2 flavor deep-elements
+call v2.bat DomWalkV2 flavor wide-attributes
+call v2.bat DomWalkV2 flavor wide-elements
+call v2.bat DomWalkV2 flavor wide-text
+call v2.bat DomWalkNLV2 flavor deep-attributes
+call v2.bat DomWalkNLV2 flavor deep-elements
+call v2.bat DomWalkNLV2 flavor wide-attributes
+call v2.bat DomWalkNLV2 flavor wide-elements
+call v2.bat DomWalkNLV2 flavor wide-text
+call v2.bat MaxMemUntypedBisLoadV2 memory 64 flavor deep-attributes
+call v2.bat MaxMemUntypedBisLoadV2 memory 128 flavor deep-attributes
+call v2.bat MaxMemUntypedBisLoadV2 memory 256 flavor deep-attributes
+call v2.bat MaxMemUntypedBisLoadV2 memory 512 flavor deep-attributes
+call v2.bat MaxMemUntypedBisLoadV2 memory 64 flavor deep-elements
+call v2.bat MaxMemUntypedBisLoadV2 memory 128 flavor deep-elements
+call v2.bat MaxMemUntypedBisLoadV2 memory 256 flavor deep-elements
+call v2.bat MaxMemUntypedBisLoadV2 memory 512 flavor deep-elements
+call v2.bat MaxMemUntypedBisLoadV2 memory 64 flavor wide-attributes
+call v2.bat MaxMemUntypedBisLoadV2 memory 128 flavor wide-attributes
+call v2.bat MaxMemUntypedBisLoadV2 memory 256 flavor wide-attributes
+call v2.bat MaxMemUntypedBisLoadV2 memory 512 flavor wide-attributes
+call v2.bat MaxMemUntypedBisLoadV2 memory 64 flavor wide-elements
+call v2.bat MaxMemUntypedBisLoadV2 memory 128 flavor wide-elements
+call v2.bat MaxMemUntypedBisLoadV2 memory 256 flavor wide-elements
+call v2.bat MaxMemUntypedBisLoadV2 memory 512 flavor wide-elements
+call v2.bat MaxMemUntypedBisLoadV2 memory 64 flavor wide-text
+call v2.bat MaxMemUntypedBisLoadV2 memory 128 flavor wide-text
+call v2.bat MaxMemUntypedBisLoadV2 memory 256 flavor wide-text
+call v2.bat MaxMemUntypedBisLoadV2 memory 512 flavor wide-text
