03653f26fec7a633cf6cc77b524e9b7a97992319
Radu Preotiuc <radup@apache.org>
2005-02-01 23:31:38 +0000

    JAM is now part of the annogen project on codehaus, with an Apache 2.0 license, so I just checked in the source code.
    git-svn-id: https://svn.apache.org/repos/asf/xmlbeans/trunk@149462 13f79535-47bb-0310-9956-ffa450edef68

--- jam/test/cases/external_jar_case/src/foo/SomeClassInAnExternalJar.java
+++
@@ -1,8 +0,0 @@
-package foo;
-
-/**
- * This class is part of the ReflectJamTest-specific test case
- * 'testClassLoaderWrapper,' which needs to test that we can reflect
- * correctly on classes in jars not in the classpath.
- */
-public class SomeClassInAnExternalJar {}
