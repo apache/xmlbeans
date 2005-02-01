2f12230e1d922bff79f3caaefffe7f39b4ee8fff
Kevin Krouse <kkrouse@apache.org>
2005-02-01 05:36:55 +0000

    bringing XMLBean ant task and docs up to date copy of r149369
    git-svn-id: https://svn.apache.org/repos/asf/xmlbeans/branches/1.x@149372 13f79535-47bb-0310-9956-ffa450edef68

--- src/xmlcomp/org/apache/xmlbeans/impl/tool/XMLBean.java
+++ src/xmlcomp/org/apache/xmlbeans/impl/tool/XMLBean.java
@@ -64,6 +64,8 @@ public class XMLBean extends MatchingTask
                         optimize,
                         download,
                         srconly,
+                        noupa,
+                        nopvr,
                         failonerror = true,
                         fork = true,
                         includeAntRuntime = true,
@@ -221,6 +223,8 @@ public class XMLBean extends MatchingTask
             params.setExtensions(extensions);
             params.setErrorListener(err);
             params.setMdefNamespaces(mdefnamespaces);
+            params.setNoUpa(noupa);
+            params.setNoPvr(nopvr);
             success = SchemaCompiler.compile(params);
 
             if (success && !srconly) {
@@ -672,6 +676,32 @@ public class XMLBean extends MatchingTask
     public void setMemoryMaximumSize(String memoryMaximumSize)
     {
         this.memoryMaximumSize = memoryMaximumSize;
+    }
+
+    /**
+     * Do not enforce the unique particle attribution rule.
+     */
+    public void setNoUpa(boolean noupa)
+    {
+        this.noupa = noupa;
+    }
+
+    public boolean isNoUpa()
+    {
+        return noupa;
+    }
+
+    /**
+     * Do not enforce the particle valid (restriction) rule.
+     */
+    public void setNoPvr(boolean nopvr)
+    {
+        this.nopvr = nopvr;
+    }
+
+    public boolean isNoPvr()
+    {
+        return nopvr;
     }
 
     private static URI uriFromFile(File f)
