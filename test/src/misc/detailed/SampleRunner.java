841d5dc1110c61439deec0e9b98bac4a4828d580
Cezar Cristian Andrei <cezar@apache.org>
2005-02-09 20:32:32 +0000

    Contributed by:Yana Kadiyska. Moving failing checkin test to detailed
    git-svn-id: https://svn.apache.org/repos/asf/xmlbeans/trunk@153115 13f79535-47bb-0310-9956-ffa450edef68

--- test/src/misc/detailed/SampleRunner.java
+++ test/src/misc/detailed/SampleRunner.java
@@ -12,7 +12,7 @@
  *   See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package misc.checkin;
+package misc.detailed;
 
 import junit.framework.TestCase;
 
