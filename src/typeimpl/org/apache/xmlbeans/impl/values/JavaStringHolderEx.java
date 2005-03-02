46132e20c0fdbc20bab806d7061b023fb481cade
Kevin Krouse <kkrouse@apache.org>
2005-03-02 23:15:05 +0000

    - fixed broken checkintests: XmlUnionImpl was always throwing XmlValueOutOfRangeException when validateOnSet was true.
    - string enums in base type are now validated against when adding facets.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/xmlbeans/trunk@155974 13f79535-47bb-0310-9956-ffa450edef68

--- src/typeimpl/org/apache/xmlbeans/impl/values/JavaStringHolderEx.java
+++ src/typeimpl/org/apache/xmlbeans/impl/values/JavaStringHolderEx.java
@@ -18,6 +18,7 @@ package org.apache.xmlbeans.impl.values;
 import org.apache.xmlbeans.SchemaType;
 import org.apache.xmlbeans.XmlObject;
 import org.apache.xmlbeans.XmlErrorCodes;
+import org.apache.xmlbeans.XmlAnySimpleType;
 import org.apache.xmlbeans.impl.common.ValidationContext;
 import org.apache.xmlbeans.impl.common.QNameHelper;
 
@@ -100,6 +101,23 @@ public abstract class JavaStringHolderEx extends JavaStringHolder
                     new Object[] { "string", new Integer(v.length()), new Integer(m), QNameHelper.readable(sType) });
                 return;
             }
+        }
+        
+        // enumeration
+        // NOTE: can't use .hasStringEnumValues() or .enumForString()
+        // here since we may be validating against a string enum value
+        // during StscSimpleTypeResolver.resolveFacets() and the string
+        // enum table hasn't been constructed yet.
+        XmlAnySimpleType[] vals = sType.getEnumerationValues();
+        if (vals != null)
+        {
+            for (int i = 0; i < vals.length; i++)
+            {
+                if (v.equals(vals[i].getStringValue()))
+                    return;
+            }
+            context.invalid(XmlErrorCodes.DATATYPE_ENUM_VALID,
+                new Object[] { "string", v, QNameHelper.readable(sType) });
         }
     }
     
