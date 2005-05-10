51982660ed735e95531e9f976d9e492fee200a1b
Cezar Cristian Andrei <cezar@apache.org>
2005-05-10 18:05:45 +0000

    Fix classpath for xpretty, scopy and dumpxsb scripts.
    git-svn-id: https://svn.apache.org/repos/asf/xmlbeans/trunk@169504 13f79535-47bb-0310-9956-ffa450edef68

--- bin/xsdtree.cmd
+++ bin/xsdtree.cmd
@@ -23,4 +23,5 @@ if "%XMLBEANS_LIB%" EQU "" call %~dp0_setlib
 
 set cp=
 set cp=%cp%;%XMLBEANS_LIB%\xbean.jar;%XMLBEANS_LIB%\jsr173_api.jar;
+
 java -classpath %cp% org.apache.xmlbeans.impl.tool.TypeHierarchyPrinter %*
