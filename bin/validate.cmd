51eb64fd54cfedf2530108dc2b1a2914d23853a4
Cezar Cristian Andrei <cezar@apache.org>
2005-07-08 20:47:18 +0000

    Contributed by: Wing Yew Poon. Fix for Jira bug 147
    git-svn-id: https://svn.apache.org/repos/asf/xmlbeans/branches/1.x-jdk1.3@209885 13f79535-47bb-0310-9956-ffa450edef68

--- bin/validate.cmd
+++ bin/validate.cmd
@@ -12,7 +12,7 @@ set cp=
 if EXIST %XMLBEANS_HOME%\build\ar\xbean.jar. (
     set cp=%XMLBEANS_HOME%\build\ar\xbean.jar;%XMLBEANS_HOME%\build\lib\xml-apis.jar;%XMLBEANS_HOME%\build\lib\xercesImpl.jar.
 ) else if EXIST %XMLBEANS_HOME%\lib\xbean.jar. (
-    set cp=%XMLBEANS_HOME%\build\lib\xbean.jar;%XMLBEANS_HOME%\lib\xml-apis.jar;%XMLBEANS_HOME%\lib\xercesImpl.jar.
+    set cp=%XMLBEANS_HOME%\lib\xbean.jar;%XMLBEANS_HOME%\lib\xml-apis.jar;%XMLBEANS_HOME%\lib\xercesImpl.jar.
 )
 
 java -classpath %cp% org.apache.xmlbeans.impl.tool.InstanceValidator %*
