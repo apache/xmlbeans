089a8aa942c5c103719afb73a8c3b7f5f62fdea7
Cezar Cristian Andrei <cezar@apache.org>
2005-08-23 00:46:03 +0000

    Contributed by Rajiv Balachandran. Changes to performance test scripts - classpath modified to reflect new xerces version used in the tests now (version 2-7-1) and adding wrapper scripts for running the tests against the various xmlbeans versions
    git-svn-id: https://svn.apache.org/repos/asf/xmlbeans/trunk@239280 13f79535-47bb-0310-9956-ffa450edef68

--- test/perf/bin/castor.bat
+++ test/perf/bin/castor.bat
@@ -57,4 +57,4 @@ goto end
 :end
 
 echo on
-java -Xmx%MEM%m -Xbootclasspath/p:%XMLBEANS_PERFROOT%\3rdparty\xerces\xerces-2_6_2\xml-apis.jar;%XMLBEANS_PERFROOT%\3rdparty\xerces\xerces-2_6_2\xercesImpl.jar -classpath %XMLBEANS_PERFROOT%\build;%XMLBEANS_PERFROOT%\schema_build\castor-purchase-order.jar;%XMLBEANS_PERFROOT%\schema_build\castor-primitives.jar;%XMLBEANS_PERFROOT%\schema_build\castor-non-primitives.jar;%XMLBEANS_PERFROOT%\3rdparty\castor\castor-0.9.5.4.jar -DPERF_ROOT=%XMLBEANS_PERFROOT% org.apache.xmlbeans.test.performance.castor.%1 %FLAVOR% %FILENAME% 
+java -Xmx%MEM%m -Xbootclasspath/p:%XMLBEANS_PERFROOT%\3rdparty\xerces\xerces-2_7_1\xml-apis.jar;%XMLBEANS_PERFROOT%\3rdparty\xerces\xerces-2_7_1\xercesImpl.jar -classpath %XMLBEANS_PERFROOT%\build;%XMLBEANS_PERFROOT%\schema_build\castor-purchase-order.jar;%XMLBEANS_PERFROOT%\schema_build\castor-primitives.jar;%XMLBEANS_PERFROOT%\schema_build\castor-non-primitives.jar;%XMLBEANS_PERFROOT%\3rdparty\castor\castor-0.9.5.4.jar -DPERF_ROOT=%XMLBEANS_PERFROOT% org.apache.xmlbeans.test.performance.castor.%1 %FLAVOR% %FILENAME% 
--- test/perf/bin/xerces.sh
+++ test/perf/bin/xerces.sh
@@ -23,4 +23,4 @@ case "$6" in
 	filename) export FILENAME=$7 ;;
 esac
 
-java -Xbootclasspath/p:$XMLBEANS_PERFROOT/3rdparty/xerces/xerces-2_6_2/xml-apis.jar:$XMLBEANS_PERFROOT/3rdparty/xerces/xerces-2_6_2/xercesImpl.jar -Xmx"$MEM"m -classpath $XMLBEANS_PERFROOT/build -DPERF_ROOT=$XMLBEANS_PERFROOT org.apache.xmlbeans.test.performance.xerces.$1 $FLAVOR $FILENAME 
+java -Xbootclasspath/p:$XMLBEANS_PERFROOT/3rdparty/xerces/xerces-2_7_1/xml-apis.jar:$XMLBEANS_PERFROOT/3rdparty/xerces/xerces-2_7_1/xercesImpl.jar -Xmx"$MEM"m -classpath $XMLBEANS_PERFROOT/build -DPERF_ROOT=$XMLBEANS_PERFROOT org.apache.xmlbeans.test.performance.xerces.$1 $FLAVOR $FILENAME 
