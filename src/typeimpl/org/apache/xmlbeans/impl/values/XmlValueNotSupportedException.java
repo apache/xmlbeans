dc53a10bce6fc0e18988cb6a68a3366a6d44afa9
Radu Preotiuc <radup@apache.org>
2005-10-18 23:22:00 +0000

    Added messages to XmlValueNotSupportedException.
    git-svn-id: https://svn.apache.org/repos/asf/xmlbeans/trunk@326301 13f79535-47bb-0310-9956-ffa450edef68

--- src/typeimpl/org/apache/xmlbeans/impl/values/XmlValueNotSupportedException.java
+++ src/typeimpl/org/apache/xmlbeans/impl/values/XmlValueNotSupportedException.java
@@ -15,8 +15,11 @@
 
 package org.apache.xmlbeans.impl.values;
 
+import org.apache.xmlbeans.XmlError;
+
 public class XmlValueNotSupportedException extends XmlValueOutOfRangeException
 {
     public XmlValueNotSupportedException() {}
     public XmlValueNotSupportedException(String message) { super(message); }
+    public XmlValueNotSupportedException(String code, Object[] args) { super(XmlError.formattedMessage(code, args)); }
 }
