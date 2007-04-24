730fe6661de0f606cba2fe002c1dd791bfa1463b
Radu Preotiuc <radup@apache.org>
2007-04-24 23:59:26 +0000

    Contributed by Andreas Loew via Wing Yew Poon. Test case for JIRA XMLBEANS-228.
    git-svn-id: https://svn.apache.org/repos/asf/xmlbeans/trunk@532146 13f79535-47bb-0310-9956-ffa450edef68

--- test/src/misc/detailed/JiraRegression201_250Test.java
+++ test/src/misc/detailed/JiraRegression201_250Test.java
@@ -17,10 +17,16 @@ package misc.detailed;
 
 import misc.common.JiraTestBase;
 import misc.detailed.jira208.FrogBreathDocument;
+import jira.xmlbeans228.substitution.PersonDocument;
+import jira.xmlbeans228.substitution.CommentType;
+import jira.xmlbeans228.substitution.FirstCommentType;
 
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Iterator;
+
+import javax.xml.namespace.QName;
 
 import org.apache.xmlbeans.XmlOptions;
 import org.apache.xmlbeans.XmlError;
@@ -78,4 +84,40 @@ public class JiraRegression201_250Test extends JiraTestBase
         assertTrue(valid2);
     }
 
+    /*
+    * [XMLBEANS-228]: 
+    * element order in sequence incorrect after calling substitute()
+    */
+    public void test_jira_xmlbeans228() throws Exception
+    {
+        PersonDocument personDocument = PersonDocument.Factory.newInstance();
+        PersonDocument.Person person = personDocument.addNewPerson();
+        CommentType commentType = person.addNewComment();
+        String ns = "http://jira/xmlbeans_228/substitution";
+        QName qName = new QName(ns, "FirstCommentElement");
+        Object resultObject = commentType.substitute(qName, FirstCommentType.type);
+        FirstCommentType firstCommentType = (FirstCommentType)resultObject;
+        firstCommentType.setStringValue("ThirdElement");
+        person.setComment(firstCommentType);
+        
+        person.setFirstName("FirstElement");
+        person.setLastName("SecondElement");
+        
+        XmlOptions opts = new XmlOptions().setSavePrettyPrint().setUseDefaultNamespace();
+        StringWriter out = new StringWriter();
+        personDocument.save(out, opts);
+
+        String exp = 
+            "<Person xmlns=\"http://jira/xmlbeans_228/substitution\">" + NEWLINE +
+            "  <FirstName>FirstElement</FirstName>" + NEWLINE +
+            "  <LastName>SecondElement</LastName>" + NEWLINE +
+            "  <FirstCommentElement>ThirdElement</FirstCommentElement>" + NEWLINE +
+            "</Person>";
+
+        assertEquals(exp, out.toString());
+        if (!personDocument.validate())
+        {
+            fail("Wrong element order!");
+        }
+    }
 }
