22c57668d16a8e9f6acf43bd9683146813aec184
PJ Fanning <pjfanning@users.noreply.github.com>
2024-06-20 10:37:02 +0100

    Update SchemaCompiler.java

--- src/main/java/org/apache/xmlbeans/impl/tool/SchemaCompiler.java
+++ src/main/java/org/apache/xmlbeans/impl/tool/SchemaCompiler.java
@@ -67,8 +67,8 @@ public class SchemaCompiler {
         System.out.println("                              processed left-to-right, e.g. \"ALL,-GET_LIST\" exclude java.util.List getters - see XmlOptions.BeanMethod" );
         System.out.println("    -repackage - repackage specification, e.g. \"org.apache.xmlbeans.metadata:mypackage.metadata\" to change the metadata directory");
         System.out.println("    -copyann - copy schema annotations to javadoc (default false) - don't activate on untrusted schema sources!");
-        System.out.println("    -sourcecodeencoding [encodingName] - Generate Java source code and jar package by the encoding specified by encodingName");
-        System.out.println("    -usejavashortname - Generate file name using JavaShortName");
+        System.out.println("    -sourcecodeencoding [encodingName] - Generate Java source code with the specified encoding (ISO-8859-1 is the legacy default)");
+        System.out.println("    -usejavashortname - Generate file name using Java Short Name");
         /* Undocumented feature - pass in one schema compiler extension and related parameters
         System.out.println("    -extension - registers a schema compiler extension");
         System.out.println("    -extensionParms - specify parameters for the compiler extension");
