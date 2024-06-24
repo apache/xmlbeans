6058cfb8b7253844bc524db389a12934aae3e56c
PJ Fanning <pjfanning@users.noreply.github.com>
2024-06-24 11:25:24 +0100

    Update XmlOptions.java

--- src/main/java/org/apache/xmlbeans/XmlOptions.java
+++ src/main/java/org/apache/xmlbeans/XmlOptions.java
@@ -156,8 +156,7 @@ public class XmlOptions implements java.io.Serializable {
         LOAD_USE_LOCALE_CHAR_UTIL,
         XPATH_USE_SAXON,
         XPATH_USE_XMLBEANS,
-        ATTRIBUTE_VALIDATION_COMPAT_MODE,
-        USE_JAVA_SHORT_NAME
+        ATTRIBUTE_VALIDATION_COMPAT_MODE
     }
 
 
