cc5a6d6968d4ddb079e20c2b53e440abd1d7fe17
aizu-m <aizumusheer2@gmail.com>
2026-06-01 11:35:35 +0530

    add tests for non-ascii digit rejection in lexInt/lexShort/lexByte

---
+++ src/test/java/misc/checkin/XsTypeConverterTest.java
@@ -0,0 +1,59 @@
+/*   Copyright 2004 The Apache Software Foundation
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package misc.checkin;
+
+import org.apache.xmlbeans.impl.util.XsTypeConverter;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class XsTypeConverterTest {
+
+    // fullwidth "123", arabic-indic "123" and devanagari "123" are unicode
+    // decimal digits but outside the xsd lexical space.
+    private static final String FULLWIDTH_123 = "１２３";
+    private static final String ARABIC_123 = "١٢٣";
+    private static final String DEVANAGARI_123 = "१२३";
+
+    @Test
+    void lexIntAcceptsAscii() {
+        assertEquals(123, XsTypeConverter.lexInt("123"));
+        assertEquals(-123, XsTypeConverter.lexInt("-123"));
+        assertEquals(123, XsTypeConverter.lexInt("+123"));
+    }
+
+    @Test
+    void lexIntRejectsNonAsciiDigits() {
+        assertThrows(NumberFormatException.class, () -> XsTypeConverter.lexInt(FULLWIDTH_123));
+        assertThrows(NumberFormatException.class, () -> XsTypeConverter.lexInt(ARABIC_123));
+        assertThrows(NumberFormatException.class, () -> XsTypeConverter.lexInt(DEVANAGARI_123));
+    }
+
+    @Test
+    void lexShortRejectsNonAsciiDigits() {
+        assertEquals(123, XsTypeConverter.lexShort("123"));
+        assertThrows(NumberFormatException.class, () -> XsTypeConverter.lexShort(FULLWIDTH_123));
+        assertThrows(NumberFormatException.class, () -> XsTypeConverter.lexShort(ARABIC_123));
+    }
+
+    @Test
+    void lexByteRejectsNonAsciiDigits() {
+        assertEquals(123, XsTypeConverter.lexByte("123"));
+        assertThrows(NumberFormatException.class, () -> XsTypeConverter.lexByte(FULLWIDTH_123));
+        assertThrows(NumberFormatException.class, () -> XsTypeConverter.lexByte(ARABIC_123));
+    }
+}
