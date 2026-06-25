9a54f5a57e7eb3e40a65272e8a27e32c0c2c536c
Aizal Khan <aizumusheer2@gmail.com>
2026-06-26 01:27:23 +0530

    report length facets in JavaNotationHolderEx.validateLexical

--- src/main/java/org/apache/xmlbeans/impl/values/JavaNotationHolderEx.java
+++ src/main/java/org/apache/xmlbeans/impl/values/JavaNotationHolderEx.java
@@ -89,7 +89,35 @@ public abstract class JavaNotationHolderEx extends JavaNotationHolder
             }
         }
 
-        check(v, sType);
+        // check against length
+        XmlObject len = sType.getFacet(SchemaType.FACET_LENGTH);
+        if (len != null)
+        {
+            int m = ((XmlObjectBase)len).getBigIntegerValue().intValue();
+            if (v.length() != m)
+                context.invalid(XmlErrorCodes.DATATYPE_LENGTH_VALID$STRING,
+                    new Object[] { "NOTATION", v.length(), m, QNameHelper.readable(sType) });
+        }
+
+        // check against min length
+        XmlObject min = sType.getFacet(SchemaType.FACET_MIN_LENGTH);
+        if (min != null)
+        {
+            int m = ((XmlObjectBase)min).getBigIntegerValue().intValue();
+            if (v.length() < m)
+                context.invalid(XmlErrorCodes.DATATYPE_MIN_LENGTH_VALID$STRING,
+                    new Object[] { "NOTATION", v.length(), m, QNameHelper.readable(sType) });
+        }
+
+        // check against max length
+        XmlObject max = sType.getFacet(SchemaType.FACET_MAX_LENGTH);
+        if (max != null)
+        {
+            int m = ((XmlObjectBase)max).getBigIntegerValue().intValue();
+            if (v.length() > m)
+                context.invalid(XmlErrorCodes.DATATYPE_MAX_LENGTH_VALID$STRING,
+                    new Object[] { "NOTATION", v.length(), m, QNameHelper.readable(sType) });
+        }
 
         return name;
     }
