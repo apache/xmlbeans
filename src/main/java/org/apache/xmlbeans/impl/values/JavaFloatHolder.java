9a8da421edced5234ab4e54869809b775a66018f
aizu-m <aizumusheer2@gmail.com>
2026-08-13 15:43:02 +0530

    report over-long float and double as invalid in validateLexical (#92)
    * report over-long float and double as invalid in validateLexical
    
    * catch RuntimeException in float and double validateLexical

--- src/main/java/org/apache/xmlbeans/impl/values/JavaFloatHolder.java
+++ src/main/java/org/apache/xmlbeans/impl/values/JavaFloatHolder.java
@@ -69,7 +69,7 @@ public abstract class JavaFloatHolder extends XmlObjectBase {
                                         int maxNumberOfChars) {
         try {
             return XsTypeConverter.lexFloat(v, strict, maxNumberOfChars);
-        } catch (NumberFormatException e) {
+        } catch (RuntimeException e) {
             context.invalid(XmlErrorCodes.FLOAT, new Object[]{v});
 
             return Float.NaN;
